e7abfb9a441e6b79d73c72de495cc33bac1606a6
JN-Jones <Jones_h1@web.de>
2014-09-17 12:00:35 +0200

    Add german

---
+++ merge/language/global.lang.php
@@ -0,0 +1,399 @@
+<?php
+
+$l['next'] = "Weiter";
+$l['version'] = "Version";
+$l['none'] = "Nichts";
+$l['please_wait'] = "Bitte warten...";
+$l['welcome'] = "Willkommen";
+$l['pause'] = "Pause";
+$l['error'] = "Fehler";
+$l['warning'] = "Warnung";
+$l['completed'] = "Fertig";
+$l['dependencies'] = "Abhängigkeiten";
+$l['resume'] = "Fortsetzen";
+$l['run'] = "Start";
+$l['cleanup'] = "Bereinigen";
+$l['yes'] = "Ja";
+$l['no'] = "Nein";
+$l['download'] = "Download";
+$l['redirecting'] = "Weiterleitung...";
+$l['dont_wait'] = "Klcke hier wenn du nicht warten möchtest.";
+$l['back'] = "Zurück";
+$l['found_error'] = "Es ist ein Fehler aufgetreten";
+$l['loading_data'] = "Lade Daten von der Datenbank...";
+$l['done'] = "Fertig";
+
+$l['module_db_configuration'] = "Datenbank Konfiguration";
+$l['module_usergroups'] = 'Benutzergruppen';
+$l['module_usergroups_singular'] = 'Benutzergruppe';
+$l['module_users'] = 'Benutzer';
+$l['module_users_singular'] = 'Benutzer';
+$l['module_categories'] = 'Kategorien';
+$l['module_categories_singular'] = 'Kategorie';
+$l['module_forums'] = 'Foren';
+$l['module_forums_singular'] = 'Forum';
+$l['module_forumperms'] = 'Forenberechtigungen';
+$l['module_forumperms_singular'] = 'Forenberechtigung';
+// Yes, this is used twice as the key used for automatic detection is different sometimes. Will be fixed in a later release
+$l['module_forum_permissions'] = 'Forenberechtigungen';
+$l['module_forum_permissions_singular'] = 'Forenberechtigung';
+$l['module_moderators'] = 'Moderatoren';
+$l['module_moderators_singular'] = 'Moderator';
+$l['module_threads'] = 'Themen';
+$l['module_threads_singular'] = 'Thema';
+$l['module_posts'] = 'Beiträge';
+$l['module_posts_singular'] = 'Beitrag';
+$l['module_attachments'] = 'Anhänge';
+$l['module_attachments_singular'] = 'Anhang';
+$l['module_polls'] = 'Umfragen';
+$l['module_polls_singular'] = 'Umfrage';
+$l['module_pollvotes'] = 'Umfrageergebnisse';
+$l['module_pollvotes_singular'] = 'Umfrageergebniss';
+// Yes, this is used twice as the key used for automatic detection is different sometimes. Will be fixed in a later release
+$l['module_poll_votes'] = 'Umfrageergebnisse';
+$l['module_poll_votes_singular'] = 'Umfrageergebniss';
+$l['module_privatemessages'] = 'Private Nachrichten';
+$l['module_privatemessages_singular'] = 'Private Nachricht';
+// Yes, this is used twice as the key used for automatic detection is different sometimes. Will be fixed in a later release
+$l['module_private_messages'] = 'Private Nachrichten';
+$l['module_private_messages_singular'] = 'Private Nachricht';
+$l['module_events'] = 'Events';
+$l['module_events_singular'] = 'Event';
+$l['module_icons'] = 'Icons';
+$l['module_icons_singular'] = 'Icon';
+$l['module_smilies'] = 'Smilies';
+$l['module_smilies_singular'] = 'Smilie';
+$l['module_settings'] = 'Einstellungen';
+$l['module_settings_singular'] = 'Einstellung';
+
+$l['creating_fields'] = "Die Felder für die Tracking Daten werden erstellt. (Dies kann eine Weile dauern)...";
+$l['creating_table'] = "Erstelle Tabelle {1}.";
+$l['creating_columns'] = "Füge {1} Spalten {2} zu Tabelle {3} hinzu";
+
+$l['indexpage_require'] = "Das MyBB Merge System benötigt MyBB 1.8.";
+
+$l['welcomepage_description'] = "Willkommen beim MyBB Merge System. Das MyBB Merge System wurde entwickelt um von anderen Softwareprodukten zum MyBB 1.8 zu konvertieren. Darüber hinaus können Sie auch mehrere Foren miteinander <i>verbinden</i>.<br /><br />Sie finden eine detaillierte Anleitung zur MyBB Merge-System auf unserer Website in der Dokumentation: ";
+$l['welcomepage_mergesystem'] = "Merge System";
+$l['welcomepage_anonymousstat'] = "Sende anonyme Statistiken an die MyBB Group";
+$l['welcomepage_informations'] = "Welche Inforamtionen werden gesendet?";
+$l['welcomepage_closeboard'] = "Schliesse dein Forum während des Merge Vorgangs";
+$l['welcomepage_note'] = "Das MyBB Merge System ist <u><strong>nicht</strong></u> zum Updaten von MyBB Foren. Darüber hinaus stellen Sie bitte sicher das alle Plugins deaktiviert sind (Alle zu verbindenden Foren). Bitte machen Sie vor der Nutzung des Merge Systems bitte ein Backup beider zu verbindenden Foren!";
+$l['welcomepage_pleasenote'] = "Bitte beachten Sie";
+
+$l['requirementspage_check'] = "Prüfung der Anforderungen";
+$l['requirementspage_req'] = "Anforderungen";
+$l['requirementspage_uptodate'] = "Aktuell";
+$l['requirementspage_outofdatedesc'] = "Das MyBB Merge System ist veraltet! Das MyBB Merge System könnte unter Umständen nicht richtig arbeiten. Aktualisieren sie es daher bitte auf die aktuelle Version: 
+";
+$l['requirementspage_outofdate'] = "Alte Version";
+$l['requirementspage_mergeoutofdate'] = "Diese Version des Merge System ist nicht aktuell";
+$l['requirementspage_unabletocheck'] = "Konnte nicht prüfen";
+$l['requirementspage_unabletocheckdesc'] = "Die Version konnte nicht vom mybb.com geprüft werden.";
+$l['requirementspage_chmoduploads'] = "Das Verzeichnis attachments  (/uploads/) ist nicht beschreibbar. Bitte prüfen Sie die ";
+$l['requirementspage_chmoduploads2'] = " Berechtigungen (chmod) der Ordner.";
+$l['requirementspage_chmod'] = "Das Verzeichnis attachments (/uploads/) ist nicht beschreibbar. Bitte prüfen Sie die ";
+$l['requirementspage_notwritable'] = "Nicht beschreibbar";
+$l['requirementspage_attnotwritable'] = "Verzeichnis für Anhänge ist nicht beschreibbar";
+$l['requirementspage_attwritable'] = "Beschreibbar";
+$l['requirementspage_attwritabledesc'] = "Verzeichnis für Anhänge ist beschreibbar";
+$l['requirementspage_reqfailed'] = "Die MyBB Merge Systemanforderungen konnten nicht geprüft werden:";
+$l['requirementspage_mergeversion'] = "Merge System Version:";
+$l['requirementspage_attwritabledesc2'] = "Verzeichnis für Anhänge beschreibbar:";
+$l['requirementspage_checkagain'] = "Klicken Sie auf, \"Erneut prüfen\" um noch einmal zu prüfen.";
+$l['requirementspage_congrats'] = "Herzlichen Glückwunsch, alle Systemanforderungen werden erfüllt! Klicke \"Weiter\" um fortzufahren.
+";
+
+$l['boardspage_welcome'] = "Vielen Dank das sie sich für MyBB entschieden haben. Dieser Assistent führt Sie durch den weiteren Verlauf des Merge Systems.";
+$l['boardspage_boardselection'] = "Foren Auswahl";
+$l['boardspage_boardselectiondesc'] = "Bitte wählen Sie das Forum von dem Sie konvertieren möchten.";
+
+$l['module_selection'] = "Modulauswahl";
+$l['module_selection_select'] = "Bitte wählen Sie die Module die gestartet werden sollen.";
+$l['module_selection_import'] = "Importiere {1} ";
+$l['module_selection_cleanup_desc'] = "Nach der Modulauswahl starten Sie bitte den nächsten Schritt der Konvertierung. Die Bereinigung wird alle temporären Daten, die während der Konvertierung erzeugt wurden, löschen.";
+
+$l['database_configuration'] = "Databank Konfiguration";
+$l['database_settings'] = "Datenbankeinstellungen";
+$l['database_engine'] = "Datenbank Engine";
+$l['database_path'] = "Datenbank Pfad";
+$l['database_host'] = "Datenbankserver ";
+$l['database_user'] = "Datenbank Benutzer";
+$l['database_pw'] = "Datenbank Passwort";
+$l['database_name'] = "Datenbank Name";
+$l['database_table_settings'] = "Tabelleneinstellungen";
+$l['database_table_prefix'] = "Tabellen Präfix";
+$l['database_table_encoding'] = "Tabellen Kodierung";
+$l['database_utf8_thead'] = "Kodiere in UTF-8";
+$l['database_utf8_desc'] = "Automatisch in UTF8 konvertieren?:<br /><small>Schalte dies aus, wenn während der Konvertierung komische Zeichen in deinem Forum erstellt werden.</small>";
+$l['database_click_next'] = "Wenn alle Einstellungen korrekt sind, klicken Sie auf Weiter.";
+$l['database_exit'] = "Konfiguration beenden";
+$l['database_check_success'] = "Prüfe Datenbankdetails... <span style=\"color: green\">Erfolgreich.</span>";
+$l['database_success'] = "Erfolgreich konfiguriert und mit der Datenbank verbunden.";
+$l['database_details'] = "Bitte geben Sie die Datenbankdaten für die Installation von {1} die sie zusammen führen möchten.";
+
+$l['wbb_installationnumber'] = "Installationsnummer";
+$l['wbb_installationnumber_desc'] = "Welche Installationsnummer wurde während der Installation ausgewählt?";
+
+$l['per_screen_config'] = "Konfiguration";
+$l['per_screen'] = "Bitte wähle aus, wie viele {1} pro Seite importiert werden sollen";
+$l['per_screen_label'] = "Anzahl {1} die gleichzeitig importiert werden";
+$l['per_screen_autorefresh'] = "Soll automatisch zur nächsten Seite weitergeleitet werden?";
+
+$l['stats_in_progress'] = "{1} {2} werden aktuell importiert. Es sind noch {3} {2} zu importieren und {4} Seiten übrig.";
+$l['stats'] = "Es werden {1} {2} importiert.";
+
+$l['progress'] = "Füge {1} #{2} ein";
+$l['progress_merging_user'] = "Verbinde Benutzer #{1} mit Benutzer #{2}";
+$l['progress_settings'] = "Füge {1} {2} von deiner {3} Datenbank ein";
+$l['progress_none_left'] = "Es gibt keine {1} mehr zu importieren. Drücke Weiter um fortzufahren.";
+$l['progress_none_left_settings'] = "Es gibt keine {1} zu updaten. Drücke Weiter um fortzufahren.";
+
+$l['import_successfully'] = "{1} erfolgreich importiert.";
+
+$l['module_post_rebuilding'] = "Zähler werden aktualisisert";
+$l['module_post_rebuild_counters'] = "<br />\nThemen, Foren und Statistik Zähler werden aktualisiert...(Dies kann etwas dauern)<br /><br />\n
+<br />\nThemen Zähler werden aktualisiert... ";
+$l['module_post_thread_counter'] = "Aktualisiere Zähler für Thema #{1}";
+$l['module_post_rebuilding_forum'] = "Foren Zähler werden aktualisiert...";
+$l['module_post_forum_counter'] = "Aktualisiere Zähler für Forum #{1}";
+$l['module_post_rebuilding_user'] = "Benutzer Zähler werden aktualisiert...";
+$l['module_post_user_counter'] = "Aktualisiere Zähler für Benutzer #{1}";
+
+$l['module_settings_updating'] = "Einstellung {1} wird aktualisiert";
+
+$l['module_attachment_link'] = "Bitte gebe den Link zum Verzeichnis der {1} Anhönge ein";
+$l['module_attachment_label'] = "Link (URL) zum Anhänge Verzeichnis";
+$l['module_attachment_error'] = "Fehler beim verschieben des Anhangs (ID: {1})";
+$l['module_attachment_not_found'] = "Fehler: konnte den Anhang nicht finden (ID: {1})";
+$l['module_attachment_thumbnail_error'] = "Fehler beim verschieben des Thumbnails (ID: {1})";
+$l['module_attachment_thumbnail_not_found'] = "Fehler: konnte das Thumbnail nicht finden (ID: {1})";
+
+$l['attmodule_notwritable'] = "Das Upload Verzeichnis (uploads/) ist nicht beschreibbar. Bitte passe die ";
+$l['attmodule_chmod'] = "chmod";
+$l['attmodule_notwritable2'] = " Berechtigungen an, um das schreiben zu erlauben.";
+$l['attmodule_ipadress'] = "Du solltest \"localhost\" nicht in der URL benutzen. Bitte nutze deine Internet IP Addresse (Stelle sicher, das Port 80 in deiner Firewall und deinem Router geöffnet ist).";
+$l['attmodule_ipadress2'] = "Du solltest \"127.0.0.1\" nicht in der URL benutzen. Bitte nutze deine Internet IP Addresse (Stelle sicher, das Port 80 in deiner Firewall und deinem Router geöffnet ist).";
+$l['attmodule_notread'] = "Die Anhänge konnten nicht gelesen werden. Bitte passe die ";
+$l['attmodule_notread2'] = " Berechtigungen an, damit von dort gelesen werden kann. Stelle auch sicher, dass die URL korrekt ist. Falls du weiterhin Probleme hast, versuche den kompletten System Pfad anstatt der URL anzugebene (zB: /var/www/htdocs/pfad/zu/deinem/alten/forum/uploads/ oder C:/pfad/zu/deinem/alten/forum/upload/). Stelle zudem sicher, dass der Zugriff nicht von einer htaccess Datei blockiert wird.";
+
+$l['removing_table'] = "Lösche Tabelle {1}.";
+$l['removing_columns'] = "Lösche Felder {1} von Tabelle {2}";
+
+$l['cleanup_header'] = "MyBB Merge System - Letzter Schritt: Bereinigen";
+$l['cleanup_notice'] = "Führe letzte Aufräumaktionen durch (Dies kann etwas dauern)...";
+
+$l['finish_completion'] = "Abschluss";
+$l['finish_head'] = '<p>Der aktuelle Merge ist jetzt beendet. Du kannst nun zu deiner <a href="../">MyBB</a> Kopie gehen oder deinem <a href="../{1}/index.php">Admin Control Panel</a>.</p>
+	<p>Bitte lösche dieses Verzeichnis falls du nicht planst weitere Foren zu verbinden.</p>';
+$l['finish_whats_next_head'] = "Was kommt als nächstes?";
+$l['finish_whats_next'] = 'Da es unmöglich ist alle Berechtigungen, Einstellungen und Zähler zu kopieren, musst du noch einige Dinge machen um sicher zu sein, dass alles wie erwartet funktioniert:
+		<ul>
+			<li>Lade alle <a href="../{1}/index.php?module=tools-cache">caches</a> neu</li>
+			<li>Lasse alle <a href="../{1}/index.php?module=tools-recount_rebuild">Neu zählen</a> Tools laufen</li>
+			<li>überprüfe alle <a href="../{1}/index.php?module=config">Einstellungen</a></li>
+			<li>überprüfe die <a href="../{1}/index.php?module=forum">Foren</a> und  <a href="../{1}/index.php?module=user-groups">Gruppen</a> Berechtigungen</li>
+		</ul>';
+$l['finish_report1'] = "Das folgende erlaubt es dir einen detaillierten Bericht der Konvertierung in verschiedenen Formaten zu downloaden.";
+$l['finish_report2'] = "Bericht Generierung";
+$l['finish_report_type'] = "Bitte wähle aus welches Format du generieren möchtest.";
+$l['finish_report_type_txt'] = "Text Datei";
+$l['finish_report_type_html'] = "HTML (im Browser anzeigbare) Datei";
+
+$l['warning_innodb'] = "Die Tabelle \"{1}\" ist momentan im InnoDB Format. Wir empfehlern diese tabellen ins MyISAM Format zu konvertieren, ansonsten kann es zu starken Verzögerungen kommen.";
+
+$l['error_invalid_board'] = "Das Forum existiert nicht.";
+$l['error_js_off'] = 'Es scheint als hättest du JavaScript ausgeschaltet. Das MyBB Merge System benötigt JavaScript um vernünftig zu funktionieren. Sobald du JavaScript eingeschaltet hast kannst du diese Seite neuladen.';
+$l['error_list'] = "Das MyBB Merge System hat folgende Probleme gefunden";
+$l['error_click_next'] = "Sobald du diese Probleme behoben hast, kannst du mit einem Klick auf \"Weiter\" fortfahren.";
+
+$l['error_database_relative'] = "Du solltest keine relativen URLs für SQLite Datanbanken benutzen. Bitte benutze den vollen System Pfad (zB /home/user/database.db) für deine SQLite Datenbank.";
+$l['error_database_invalid_engine'] = "Du hast eine ungültige Datenbank Engine ausgewählt. Stelle sicher, dass du aus der Liste unten auswählst.";
+$l['error_database_cant_connect'] = "Konnte mit den gegebenen Daten nicht mit dem Datenbankserver auf '{1}' verbinden. Bist du dir sicher, dass Host und Benutzerdaten korrekt sind?";
+$l['error_database_wrong_table'] = "Die {1} Datenbank konnte nicht auf '{2}' gefunden werden. Bitte stelle sicher, dass {1} in dieser Datenbank mit diesem Tabellenpräfix existiert.";
+$l['error_database_list'] = "Es scheint Probleme mit den gegeben Datenbank Informationen zu geben";
+$l['error_database_continue'] = "Sobald die Fehler behoben sind kannst du fortfahren.";
+
+$l['loginconvert_title'] = "MyBB Merge System - Passwort Konvertierung";
+$l['loginconvert_message'] = "			<div class=\"error\">\n
+				<h3>Fehler</h3>
+				Das MyBB Merge System kann nicht fortfahren, bis du loginconvert.php (zu finden in diesem Verzeichnis) in deinen Plugin Ordner (inc/plugins) hochgeladen hast.\n
+		</div>
+
+		<p>Mehr Informationen findest du <a href=\"http://docs.mybb.com/1.8/merge/running/#loginconvert.php-plugin\" target=\"_blank\">hier</a>.</p>
+		<p>Sobald du die Datei hochgeladen ist kannst du auf Weiter klicken.</p>";
+
+
+$l['report_txt'] = 'MyBB Merge System - Merge Report
+--------------------------------------------------------
+Welcome to the MyBB Merge System Generated Report. This
+report shows a small overview of this merge session.
+
+General
+-------
+	Board merged:    {1}
+	Import began:    {2}
+	Import finished: {3}
+
+Database Query Statistics
+-------------------------
+	Queries on MyBB database: {4}
+	Queries on old database:  {5}
+	Total query time:         {6}
+
+Modules
+-------
+The following modules from this converter were completed:
+{7}
+
+Import Statistics
+-----------------
+The MyBB import system imported the following from your copy of {8}:
+{9}
+
+Errors
+------
+The following errors were logged during the process of the Merge System:
+{10}
+
+Problems?
+---------
+The "mybb_debuglogs" table located in your database contains
+debug information about this merge. If you find problems
+please file a support inquery at http://community.mybb.com/.
+
+--------------------------------------------------------
+Generated: {11}';
+
+$l['report_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>MyBB Merge System &gt; Generated Report</title>
+	<style type="text/css">
+		body {
+			font-family: Verdana, Arial, sans-serif;
+			font-size: 12px;
+			background: #efefef;
+			color: #000000;
+			margin: 0;
+		}
+
+		#container {
+			margin: auto auto;
+			width: 780px;
+			background: #fff;
+			border: 1px solid #ccc;
+			padding: 20px;
+		}
+
+		h1 {
+			font-size: 25px;
+			margin: 0;
+			background: #ddd;
+			padding: 10px;
+		}
+
+		h2 {
+			font-size: 18px;
+			margin: 0;
+			padding: 10px;
+			background: #efefef;
+		}
+
+		h3 {
+			font-size: 14px;
+			clear: left;
+			border-bottom: 1px dotted #aaa;
+			padding-bottom: 4px;
+		}
+
+		ul, li {
+			padding: 0;
+		}
+
+		#general p, #modules p, #import p, ul, li, dl {
+			margin-left: 30px;
+		}
+
+		dl dt {
+			float: left;
+			width: 300px;
+			padding-bottom: 10px;
+			font-weight: bold;
+		}
+
+		dl dd {
+			padding-bottom: 10px;
+		}
+
+		#footer {
+			border-top: 1px dotted #aaa;
+			padding-top: 10px;
+			font-style: italic;
+		}
+
+		.float_right {
+			float: right;
+		}
+	</style>
+</head>
+<body>
+<div id="container">
+	<h1>MyBB Merge System</h1>
+	<h2>Merge Report</h2>
+	<p>Welcome to the MyBB Merge System Generated Report. This report shows a small overview of this merge session.</p>
+	<div id="general">
+		<h3>General Statistics</h3>
+		<p>You merged {1} to your forum.</p>
+		<dl>
+			<dt>Import began</dt>
+			<dd>{2}</dd>
+
+			<dt>Import finished</dt>
+			<dd>{3}</dd>
+		</dl>
+	</div>
+	<div id="database">
+		<h3>Database Query Statistics</h3>
+		<dl>
+			<dt>Queries on the MyBB database</dt>
+			<dd>{4}</dd>
+
+			<dt>Queries on the {8} database</dt>
+			<dd>{5}</dd>
+
+			<dt>Total query time</dt>
+			<dd>{6}</dd>
+		</dl>
+	</div>
+	<div id="modules">
+		<h3>Modules</h3>
+		<p>The following modules from this converter were completed:</p>
+		<ul>
+		{7}
+		</ul>
+	</div>
+	<div id="import">
+		<h3>Import Statistics</h3>
+		<p>The MyBB import system imported the following from your copy of {8}:</p>
+		<dl>
+		{9}
+		</dl>
+	</div>
+	<div id="errors">
+		<h3>Errors</h3>
+		<p>The following errors were logged during the process of the Merge System:</p>
+		<ul>
+		{10}
+		</ul>
+	</div>
+	<div id="problems">
+		<h3>Problems?</h3>
+		<p>The "mybb_debuglogs" table located in your database contains debug information about this merge. If you find problems please file a support inquiry at the <a href="http://community.mybb.com/">MyBB Community Forums</a>.</p>
+	</div>
+	<div id="footer">
+		<div class="float_right">MyBB &copy; 2002-{12} MyBB Group</div>
+		<div>Generated {11}</div>
+	</div>
+</div>
+</body>
+</html>';
